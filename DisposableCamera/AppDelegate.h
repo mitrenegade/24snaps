6d74ef3c35b8366926fa408ca765f00a58fc74b5
mitrenegade <bobbyren@gmail.com>
2014-12-29 15:51:28 -0500

    Initial Commit

---
+++ DisposableCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DisposableCamera
+//
+//  Created by Bobby Ren on 12/29/14.
+//  Copyright (c) 2014 Bobby Ren Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DisposableCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DisposableCamera
+//
+//  Created by Bobby Ren on 12/29/14.
+//  Copyright (c) 2014 Bobby Ren Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
